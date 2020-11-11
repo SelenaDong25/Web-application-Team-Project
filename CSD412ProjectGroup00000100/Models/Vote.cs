48e0753afe7d73ff19ad96f638e70bd949b0165c
Bingo <bingo.dc19@gmail.com>
2020-11-11 06:03:50 -0800

    Create Vote.cs

---
+++ CSD412ProjectGroup00000100/Models/Vote.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSD412ProjectGroup00000100.Models
+{
+    public class Vote
+    {
+    }
+}
