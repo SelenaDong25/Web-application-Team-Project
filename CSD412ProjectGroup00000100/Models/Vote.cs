5578914ce1e4232635de511ae54354d7331810b5
Bingo <bingo.dc19@gmail.com>
2020-11-11 07:30:55 -0800

    Update Vote.cs
    updated Vote Class so that it has atrubutes however we are lacking a whole buch of composite keys which it badly needs

--- CSD412ProjectGroup00000100/Models/Vote.cs
+++ CSD412ProjectGroup00000100/Models/Vote.cs
@@ -1,11 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CSD412ProjectGroup00000100.Models
 {
     public class Vote
-    {
+    { 
+        private int _item_id;
+        private int _poll_id;
+        private int _user_id;
+        private DateTime _vote_date_time;
+        //ToDo figure out how to do a composite ForeingKey key int .net
+        //[Key,ForeignKey("Item")]
+        [Key]
+        public int item_id
+        {
+            get { return _item_id; }
+            set { _item_id = value; }
+        }
+        //[Key,ForeignKey("Poll")]
+        public int poll_id
+        {
+            get { return _poll_id; }
+            set { _poll_id = value; }
+        }
+        //[Key,ForeignKey("User???")] 
+        public int user_id
+        {
+            get { return _user_id; }
+            set { _user_id = value; }
+        }
+        public DateTime vote_date_time 
+        {
+            get { return _vote_date_time; }
+            set { _vote_date_time = value; }
+        }
     }
 }
