548fbe6e4bcf5539507182bc78b2f27a91473e27
Bingo <bingo.dc19@gmail.com>
2020-11-11 07:29:03 -0800

    Update Item.cs
    add atrubutes to the time class however need to figure out how to do a comopite key on the poll_id

--- CSD412ProjectGroup00000100/Models/Item.cs
+++ CSD412ProjectGroup00000100/Models/Item.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,31 @@ namespace CSD412ProjectGroup00000100.Models
 {
     public class Item
     {
+        private int _item_id;
+        private int _poll_id;
+        private string _name;
+        private String _description;
+        [Key]
+        public int item_id
+        {
+            get { return _item_id; }
+            set { _item_id = value; }
+        }
+        //[Key,ForeignKey("Poll")]
+        public int poll_id
+        {
+            get { return _poll_id; }
+            set { _poll_id = value; }
+        }
+        public String name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public String description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
     }
 }
