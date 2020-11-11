51eddd129703bb50b94f5e9baad150ed6a766fd2
Bingo <bingo.dc19@gmail.com>
2020-11-11 07:42:24 -0800

    made a Base Migration for the new atrubutes in our Class
    setup migtation for the Modle Class in project

--- CSD412ProjectGroup00000100/Data/Migrations/20201111153747_BaseMigration.Designer.cs
+++ CSD412ProjectGroup00000100/Data/Migrations/20201111153747_BaseMigration.Designer.cs
@@ -4,14 +4,16 @@ using CSD412ProjectGroup00000100.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CSD412ProjectGroup00000100.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201111153747_BaseMigration")]
+    partial class BaseMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
