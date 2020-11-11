cc48e21ed5f7f45a55b0ee0c1763fc35cc1177a5
Bingo <bingo.dc19@gmail.com>
2020-11-11 07:27:51 -0800

    Update Poll.cs
    updated poll with a whole buch of of atributes along with Keys however  need to figure out how to to compostite key on user_id

--- CSD412ProjectGroup00000100/Models/Poll.cs
+++ CSD412ProjectGroup00000100/Models/Poll.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,37 @@ namespace CSD412ProjectGroup00000100.Models
 {
     public class Poll
     {
+       private int _poll_id;
+       private int _user_id;
+       private String _name;
+       private String _description;
+       private Boolean _state;
+       [Key]
+       public int poll_id
+       {
+            get { return _poll_id; }
+            set { _poll_id = value; }
+       }
+        //[Key,ForeignKey("User???")] 
+        public int user_id
+        {
+            get { return _user_id; }
+            set { _user_id = value; }
+        }
+        public String name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public String description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        public Boolean state
+        {
+            get { return _state; }
+            set { _state = value; }
+        }
     }
 }
