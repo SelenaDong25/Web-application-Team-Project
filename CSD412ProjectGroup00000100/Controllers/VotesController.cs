05b12c3dac017cafad2001f1e132b0bf8284ed75
Bingo <bingo.dc19@gmail.com>
2020-11-19 22:04:04 -0800

    made controlers and views for votes

---
+++ CSD412ProjectGroup00000100/Controllers/VotesController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CSD412ProjectGroup00000100.Data;
+using CSD412ProjectGroup00000100.Models;
+
+namespace CSD412ProjectGroup00000100.Controllers
+{
+    public class VotesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VotesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Votes
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Votes.Include(v => v.Item).Include(v => v.Voter);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Votes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vote = await _context.Votes
+                .Include(v => v.Item)
+                .Include(v => v.Voter)
+                .FirstOrDefaultAsync(m => m.VoteId == id);
+            if (vote == null)
+            {
+                return NotFound();
+            }
+
+            return View(vote);
+        }
+
+        // GET: Votes/Create
+        public IActionResult Create()
+        {
+            ViewData["ItemId"] = new SelectList(_context.Items, "ItemId", "ItemId");
+            ViewData["VoterId"] = new SelectList(_context.Set<ApplicationUser>(), "Id", "Id");
+            return View();
+        }
+
+        // POST: Votes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("VoteId,ItemId,VoterId,VoteDateTime")] Vote vote)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(vote);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ItemId"] = new SelectList(_context.Items, "ItemId", "ItemId", vote.ItemId);
+            ViewData["VoterId"] = new SelectList(_context.Set<ApplicationUser>(), "Id", "Id", vote.VoterId);
+            return View(vote);
+        }
+
+        // GET: Votes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vote = await _context.Votes.FindAsync(id);
+            if (vote == null)
+            {
+                return NotFound();
+            }
+            ViewData["ItemId"] = new SelectList(_context.Items, "ItemId", "ItemId", vote.ItemId);
+            ViewData["VoterId"] = new SelectList(_context.Set<ApplicationUser>(), "Id", "Id", vote.VoterId);
+            return View(vote);
+        }
+
+        // POST: Votes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("VoteId,ItemId,VoterId,VoteDateTime")] Vote vote)
+        {
+            if (id != vote.VoteId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vote);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VoteExists(vote.VoteId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ItemId"] = new SelectList(_context.Items, "ItemId", "ItemId", vote.ItemId);
+            ViewData["VoterId"] = new SelectList(_context.Set<ApplicationUser>(), "Id", "Id", vote.VoterId);
+            return View(vote);
+        }
+
+        // GET: Votes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vote = await _context.Votes
+                .Include(v => v.Item)
+                .Include(v => v.Voter)
+                .FirstOrDefaultAsync(m => m.VoteId == id);
+            if (vote == null)
+            {
+                return NotFound();
+            }
+
+            return View(vote);
+        }
+
+        // POST: Votes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var vote = await _context.Votes.FindAsync(id);
+            _context.Votes.Remove(vote);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VoteExists(int id)
+        {
+            return _context.Votes.Any(e => e.VoteId == id);
+        }
+    }
+}
