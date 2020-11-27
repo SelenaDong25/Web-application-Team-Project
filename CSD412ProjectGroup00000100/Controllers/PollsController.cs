1984df955878b2939303554c97ac694411f2b6e2
Bingo <bingo.dc19@gmail.com>
2020-11-27 02:19:51 -0800

    add pie api

--- CSD412ProjectGroup00000100/Controllers/PollsController.cs
+++ CSD412ProjectGroup00000100/Controllers/PollsController.cs
@@ -9,6 +9,8 @@ using CSD412ProjectGroup00000100.Data;
 using CSD412ProjectGroup00000100.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
+using System.Data;
+using System.Collections;
 
 namespace CSD412ProjectGroup00000100.Controllers
 {
@@ -39,19 +41,58 @@ namespace CSD412ProjectGroup00000100.Controllers
         // GET: Polls/Details/5
         public async Task<IActionResult> Details(int? id)
         {
+            ApplicationUser theUser = await _userManager.GetUserAsync(User);
             if (id == null)
             {
                 return NotFound();
             }
 
-            var poll = await _context.Polls
-                .Include(p => p.Items)
-                .FirstOrDefaultAsync(m => m.PollId == id);
+            var poll = await _context.Polls.Include(p => p.Items).FirstOrDefaultAsync(m => m.PollId == id);
             if (poll == null)
             {
                 return NotFound();
             }
+
+            
             var model = new ViewPoll { CurrentPoll = poll, Items = poll.Items.ToArray() };
+
+            // check if the user has any votes 
+            foreach (Item item in poll.Items.ToArray())
+            {
+                var vote = _context.Votes.Include(p => p.Item).Where(x => x.Voter == theUser && x.Item == item);
+                if (vote.Count() >= 1) 
+                {
+                    // a vote exists set show results to true  
+                    model.ShowResult = true;
+                    break;
+                }
+            }  
+            // if there is any votes result 
+            if (model.ShowResult) {
+                List<object> iData = new List<object>();
+                DataTable dt = new DataTable();
+                dt.Columns.Add("Item Name", System.Type.GetType("System.String"));
+                dt.Columns.Add("Votes", System.Type.GetType("System.Int32"));
+                dt.Columns.Add("Color", System.Type.GetType("System.String"));
+                foreach (Item item in poll.Items.ToArray())
+                {
+                    DataRow dr = dt.NewRow();
+                    dr["Item Name"] = item.Name;
+                    dr["Votes"] = _context.Votes.Include(p => p.Item).Where(x => x.Item == item).Count();// test value 
+                    dr["Color"] = item.ColorValue;
+                    dt.Rows.Add(dr);
+                }
+                foreach (DataColumn dc in dt.Columns)
+                {
+                    List<object> x = new List<object>();
+                    x = (from DataRow drr in dt.Rows select drr[dc.ColumnName]).ToList();
+                    iData.Add(x);
+                }
+                ViewBag.labels = iData.ElementAt(0);
+                ViewBag.data = iData.ElementAt(1);
+                ViewBag.color = iData.ElementAt(2);
+                model.IData = iData;
+            }
             return View(model);
         }
 
--- CSD412ProjectGroup00000100/Models/ViewPoll.cs
+++ CSD412ProjectGroup00000100/Models/ViewPoll.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +11,9 @@ namespace CSD412ProjectGroup00000100.Models
         private Boolean _showResult = false;
         public IEnumerable<Item> Items { get; set; }
         public Poll CurrentPoll { get; set; }
+
+        // stores all the results of the poll
+        public List<object> IData { get; set; }
         public Boolean ShowResult
         {
             get { return _showResult; }
