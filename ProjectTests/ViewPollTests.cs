c0fe8ad53463bc301be129a22a9f72d996db6627
Bingo <bingo.dc19@gmail.com>
2020-11-28 07:02:29 -0800

    Update ViewPollTests.cs
    so that ViewPoll has a thory test

--- ProjectTests/ViewPollTests.cs
+++ ProjectTests/ViewPollTests.cs
@@ -1,17 +1,33 @@
 ﻿using CSD412ProjectGroup00000100.Models;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace ProjectTests
 {
     public class ViewPollTests
     {
+
         [Fact]
         public void Test1()
         {
             ViewPoll vp = new ViewPoll();
             Assert.True(vp.ShowResult == false);
         }
-  
+
+        [Theory]
+        [InlineData( "Test", "Test", "Test", "Test")]
+        [InlineData("", "", "", "")]
+        [InlineData("hohohohoho", "ohohohohohoh", "hohohohoho", "ohohohohohoh")]
+        [InlineData("", "poooooooooooooooooooooooooooooooooooooooooooooop"
+                   , "", "poooooooooooooooooooooooooooooooooooooooooooooop")]
+        [InlineData("poooooooooooooooooooooooooooooooooooooooooooooop",""
+                   , "poooooooooooooooooooooooooooooooooooooooooooooop","")]
+        public void Test2(string name, string description, string expectedName, string expectedDescription)
+        {
+            Poll p = new Poll {Name = name, Description = description}; 
+            ViewPoll vp = new ViewPoll {CurrentPoll = p };
+            Assert.True(vp.CurrentPoll.Description == expectedDescription && vp.CurrentPoll.Name == expectedName);
+        }
     }
 }
